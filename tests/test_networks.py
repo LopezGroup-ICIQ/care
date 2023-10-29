9093b1e0ec3e98182658b19ebfbbd9ffcb11a5c3
SMorandi95 <smorandi@iciq.es>
2023-10-29 20:30:07 +0100

    cleaned tests

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -5,8 +5,15 @@ from GAMERNet.rnet.networks.elementary_reaction import ElementaryReaction
 from GAMERNet.rnet.networks.intermediate import Intermediate
 
 class TestElementaryReaction(unittest.TestCase):
-	def test_stoic(self):
-		step = ElementaryReaction()
+	pass
+
+class TestIntermediate(unittest.TestCase):
+	pass
+
+class TestReactionNetwork(unittest.TestCase):
+	pass
+
+
 		
 
 	
