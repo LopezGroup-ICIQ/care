b6d31dbacc9f0f25fee2822a03c6f820e8af6e6b
SMorandi95 <smorandi@iciq.es>
2023-10-29 18:43:19 +0100

    trying to fix github workflow

--- create_env.sh
+++ create_env.sh
@@ -2,19 +2,21 @@
 
 
 # Create the conda environment from environment.yml
-conda env create -f environment.yml
+conda env create -n care -f environment.yml
 
 # Activate the newly created environment
-conda activate $ENV_NAME
+conda init bash
+conda activate care
 
 # Install any additional pip packages if needed
 pip install acat  
 pip install pubchempy
 pip install rdkit
+pip install flake8
 pip install git+https://github.com/giacomomarchioro/PyEnergyDiagrams
 
 pip install -e .
 
 # Notify the user that the environment setup is complete
-echo "Conda environment '$ENV_NAME' is set up and activated."
+echo "Conda environment 'care' is set up and activated."
 
