1025e747d7637d2d976d577a2065fcc44e2fa2dd
Oliver Loveday <oliverlovros@gmail.com>
2023-11-19 22:36:02 +0100

    succesfully implemented code rdkit

--- scripts/test_implement_smiles.py
+++ scripts/test_implement_smiles.py
@@ -2,8 +2,10 @@ import os
 import multiprocessing as mp
 from rdkit.Chem import rdMolDescriptors
 from rdkit import Chem
+from rdkit.Chem import Draw
+from PIL import Image
 
-from care.rnet.intermediates_funcs import gen_alkanes_smiles, add_oxygens_to_molecule, gen_ether_smiles, rdkit_2_graph, id_group_dict, process_molecule
+from care.rnet.intermediates_funcs import gen_alkanes_smiles, add_oxygens_to_molecule, gen_ether_smiles, rdkit_2_graph, id_group_dict, process_molecule, gen_epoxides_smiles
 from care.rnet.utilities.functions import MolPack
 
 import warnings
@@ -74,32 +76,16 @@ def find_unique_bonds(mol):
 
 # Initialize a set for unique reactions
 unique_reactions = set()
+processed_molecules = set()
 def break_bonds(molecule: Chem.rdchem.Mol, bond_types: list[tuple[int, int]], processed_fragments: dict, original_smiles: str):
-    """
-    Break the bonds of a molecule and recursively call itself on the fragments
-    
-    Parameters
-    ----------
-    molecule : rdkit.Chem.rdchem.Mol
-        The molecule to break the bonds of
-    bond_types : list
-        A list of tuples of the desired bond types
-    processed_fragments : dict
-        A dictionary of processed fragments
-    original_smiles : str
-        The original SMILES string of the molecule
-        
-    Returns
-    -------
-        None
-    """
-
-    # Convert the current molecule to SMILES at this depth
     current_smiles = Chem.MolToSmiles(molecule, isomericSmiles=True, allHsExplicit=True)
 
-    # Finding unique bonds
-    unique_bonds = find_unique_bonds(molecule)
+    # Check if this molecule's reactions have already been processed
+    if current_smiles in processed_molecules:
+        return 0
 
+    unique_bonds = find_unique_bonds(molecule)
+    
     total_bond_counter = 0
     for bond in unique_bonds:
         for atom_num1, atom_num2 in bond_types:
@@ -136,6 +122,8 @@ def break_bonds(molecule: Chem.rdchem.Mol, bond_types: list[tuple[int, int]], pr
                     reaction_str = f"{current_formula} -> {' + '.join(frag_formula_list)}, : {bond.GetBeginAtom().GetSymbol()}-{bond.GetEndAtom().GetSymbol()} bond"
                     print(reaction_str)
                     total_bond_counter += 1
+        
+        processed_molecules.add(current_smiles)
 
     return total_bond_counter
 
@@ -177,17 +165,19 @@ def generate_intermediates(ncc: int, noc: int,) -> dict[str, dict[int, list[MolP
     alkanes_smiles = gen_alkanes_smiles(ncc)    
     mol_alkanes = [Chem.MolFromSmiles(smiles) for smiles in list(alkanes_smiles)]
     print("Done generating saturated CHO molecules.")
+    ethers_smiles = gen_ether_smiles(mol_alkanes, noc)
+    alkanes_smiles += ethers_smiles
+    mol_alkanes_ethers = mol_alkanes + [Chem.MolFromSmiles(smiles) for smiles in list(ethers_smiles)]
     # 2) Add oxygens to each molecule
     print("Adding oxygens to molecules...")
-    cho_smiles = [add_oxygens_to_molecule(mol, noc) for mol in mol_alkanes] 
+    cho_smiles = [add_oxygens_to_molecule(mol, noc) for mol in mol_alkanes_ethers] 
     cho_smiles = [smiles for smiles_set in cho_smiles for smiles in smiles_set]  # flatten list of lists
-    #epoxides_smiles = gen_epoxides_smiles(mol_alkanes, noc)
+    epoxides_smiles = gen_epoxides_smiles(mol_alkanes, noc)
     print("Done adding oxygens to molecules.")
     # 3) Add ethers to each molecule
     print("Adding ethers to molecules...")
-    ethers_smiles = gen_ether_smiles(mol_alkanes, noc)
-    #cho_smiles += epoxides_smiles
-    cho_smiles += ethers_smiles
+    cho_smiles += epoxides_smiles
+    # cho_smiles += ethers_smiles
     print("Done adding ethers to molecules.")
     cho_smiles += ['CO','C(O)O','O', 'OO', '[H][H]']
     all_smiles = cho_smiles + alkanes_smiles
@@ -201,11 +191,45 @@ def generate_intermediates(ncc: int, noc: int,) -> dict[str, dict[int, list[MolP
         print(smiles)
         n_react = process_molecule(smiles, bond_types, processed_fragments)
     print(len(unique_reactions))
-    
+    return all_smiles
 
-generate_intermediates(ncc=3, noc=-1)
+all_smiles = generate_intermediates(ncc=7, noc=-1)
+
+# Sorting the unique reactions by length of the reactants
+unique_reactions = sorted(unique_reactions, key=lambda x: len(x[0]))
+
 
 # Saving the unique reactions in a text file
 with open('unique_reactions.txt', 'w') as f:
     for reaction in unique_reactions:
         f.write(f'{reaction[0]} -> {reaction[1]}\n')
+
+# # Function to count carbons in a SMILES string
+# def count_carbons(smiles):
+#     mol = Chem.MolFromSmiles(smiles)
+#     return sum(1 for atom in mol.GetAtoms() if atom.GetSymbol() == 'C')
+
+# # Sort all_smiles by the number of carbons
+# sorted_smiles = sorted(all_smiles, key=count_carbons)
+
+# # Image Generation options
+# options = Draw.MolDrawOptions()
+# options.includeAtomNumbers = False
+# options.explicitMethyl = True
+# options.explicitOnly = True
+# options.explicitHs = True
+
+# # Determine the number of rows and columns for the grid
+# grid_rows = len(sorted_smiles)
+# grid_cols = 1  # Each molecule in its own row
+# grid_image = Image.new('RGB', (200 * grid_cols, 200 * grid_rows), 'white')
+
+# # Populate the grid
+# for row_index, smiles in enumerate(sorted_smiles):
+#     mol = Chem.MolFromSmiles(smiles)
+#     img = Draw.MolToImage(mol, size=(200, 200), options=options)
+#     grid_image.paste(img, (0, row_index * 200))  # Always in the first column
+
+# # Save or display the grid image
+# grid_image.save("molecules_sorted_by_carbons.png")
+# grid_image.show()
