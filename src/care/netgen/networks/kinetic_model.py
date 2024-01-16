c32933979e0a9881747acd7687cce1fa50ec1789
SMorandi95 <smorandi@iciq.es>
2024-01-16 19:54:11 +0100

    Removed netgen

--- src/care/netgen/networks/kinetic_model.py
+++
@@ -1,7 +0,0 @@
-from care.netgen.networks.reaction_network import ReactionNetwork
-
-class KineticModel:
-    def __init__(self, rxn_net: ReactionNetwork):
-        self.rxn_net = rxn_net
-        self.rxn_net.generate_kinetic_model()
-        self.kinetic_model = self.rxn_net.kinetic_model
