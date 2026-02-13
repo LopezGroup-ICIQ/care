068e31231d221f5266854bbd800fe1a41277bef4
SMorandi95 <smorandi@iciq.es>
2026-02-13 11:02:18 +0100

    fix test io

--- tests/test_io.py
+++ tests/test_io.py
@@ -5,7 +5,7 @@ from tests import TEST_DIR
 
 class TestIO(unittest.TestCase):
     def test_reconstructed_network(self):
-        crn = load_network(str(TEST_DIR) + "/files/c1o2_Ru0001.gz")
-        save_network(crn, str(TEST_DIR) + "/files/c1o2_Ru0001_new.gz")
-        crn_reconstructed = load_network(str(TEST_DIR) + "/files/c1o2_Ru0001_new.gz")
+        crn = load_network(str(TEST_DIR) + "/files/c1o2_Ru0001.json.gz")
+        save_network(crn, str(TEST_DIR) + "/files/c1o2_Ru0001_new.json")
+        crn_reconstructed = load_network(str(TEST_DIR) + "/files/c1o2_Ru0001_new.json.gz")
         self.assertEqual(crn, crn_reconstructed)
